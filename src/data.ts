64999bb490a7c4e47026aa79d65be064e141a751
堀江岳浩 <horie.takehiro@ntt-tx.co.jp>
2025-09-22 13:37:05 +0900

    add error message

--- src/data.ts
+++ src/data.ts
@@ -29,11 +29,45 @@ export class Repository {
   };
 }
 const client = generateClient<Schema>();
-// repository for production 
+/**
+ * this works (pass tsc)
+ */
 export const productionRepository = new Repository({
   getTenant: client.models.Tenant.get,
 })
-// repository for tests
+/**
+ * this cause `TS2322` error (tsc failed)
+ * ```
+ * src/data.ts:62:33 - error TS2322: Type 'Promise<{ data: { id: string; name: string; }; }>' is not assignable to type 'SingularReturnValue<Prettify<ReturnValue<ClientModel<{ Tenant: ClientModel<..., SchemaMetadata<ModelSchema<SetTypeSubArg<{ types: { Tenant: ModelType<{ fields: { name: ModelField<string, "required", undefined>; }; identifier: ModelDefaultIdentifier; secondaryIndexes: []; authorization: []; disabledOperations: []; },...'.
+  Type '{ data: { id: string; name: string; }; }' is not assignable to type '{ data: Prettify<ReturnValue<ClientModel<{ Tenant: ClientModel<..., SchemaMetadata<ModelSchema<SetTypeSubArg<{ types: { Tenant: ModelType<{ fields: { name: ModelField<string, "required", undefined>; }; identifier: ModelDefaultIdentifier; secondaryIndexes: []; authorization: []; disabledOperations: []; }, never>; }; ...'.
+    Types of property 'data' are incompatible.
+      Type '{ id: string; name: string; }' is not assignable to type 'Prettify<ReturnValue<ClientModel<{ Tenant: ClientModel<..., SchemaMetadata<ModelSchema<SetTypeSubArg<{ types: { Tenant: ModelType<{ fields: { name: ModelField<string, "required", undefined>; }; identifier: ModelDefaultIdentifier; secondaryIndexes: []; authorization: []; disabledOperations: []; }, never>; }; authoriz...'.
+
+ 62   getTenant: async (...args) => ({
+                                    ~~
+ 63     data: {
+    ~~~~~~~~~~~
+... 
+ 70     },
+    ~~~~~~
+ 71   }),
+    ~~~~
+
+  node_modules/@aws-amplify/data-schema/dist/esm/runtime/client/index.d.ts:318:5
+    318     get<SelectionSet extends ReadonlyArray<ModelPath<FlatModel>> = never[]>(identifier: Model['identifier'], options?: {
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    319         selectionSet?: SelectionSet;
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ... 
+    322         headers?: CustomHeaders;
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    323     }): SingularReturnValue<Prettify<ReturnValue<Model, FlatModel, SelectionSet>>>;
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    The expected type comes from the return type of this signature.
+  Found 1 error in src/data.ts:62
+ * ```
+ * 
+ */
 export const dummyRepository = new Repository({
   getTenant: async (...args) => ({
     data: {
